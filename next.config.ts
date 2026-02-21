3ce74e0e8bbae8d5cd06bca2c8706006d3d54799
Faiz Favaz <faizfavaz7@gmail.com>
2026-02-21 15:54:57 +0530

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Disable ESLint errors from breaking builds
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
